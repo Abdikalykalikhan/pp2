ee821249086835d6f62f180e1d02f65d5ea79b75
Alikhan Abdikalyk <a_abdikalyk@kbtu.kz>
2024-02-25 18:22:18 +0600

    laba5

--- laba5/regex/regex8.py
+++ laba5/regex/regex8.py
@@ -1,14 +1,14 @@
 import re
 
 def test(pattern, testData, expectedResult):
-    result = re.split(pattern, testData)
+    result = re.sub(pattern, r'\1 \2', testData)
     print(result)
     if result == expectedResult:
         print("test is passed!")
     else: 
         print("test is not passed!")
 
-pattern = r'([A-Z][a-z]*)'
+pattern = r'([a-zA-Z])([A-Z])'
 
-test(pattern, "MySuperTest", "['', 'My', '', 'Super', '', 'Test', '']")
-test(pattern, " MySuperTest IAmRobot", "[' ', 'My', '', 'Super', '', 'Test', ' ', 'I', '', 'Am', '', 'Robot', '']")
+test(pattern, "MySuperTest", "My Super Test")
+test(pattern, " MySuperTest IAmRobot", " My Super Test I Am Robot")
