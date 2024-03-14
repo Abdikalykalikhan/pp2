fc42dec926a6feb33ac563b32c138eab8b79a811
Alikhan Abdikalyk <a_abdikalyk@kbtu.kz>
2024-03-15 03:50:28 +0600

    laba7

--- laba7/music_player/musica.py
+++ laba7/music_player/musica.py
@@ -1,31 +1,37 @@
 from msvcrt import getch
 import pygame
 
-_songs = ['song.mp3']
+songs = ['song.mp3', 'Arcade.mp3', 'Another_Love.mp3', 'Sweater_Weather.mp3']
 pygame.mixer.init()
-'''pygame.mixer.music.load(songs)'''
-
 
 def play_next_song():
-    global _songs
-    _songs = _songs[1:] + [_songs[0]] # move current song to the back of the list
-    pygame.mixer.music.load(_songs[0])
+    global songs
+    songs = songs[1:] + [songs[0]] # move current song to the back of the list
+    pygame.mixer.music.load(songs[0])
     pygame.mixer.music.play()
+
+def play_last_song():
+    global songs
+    songs = [songs[-1]] + songs[:-1] # move current song to the back of the list
+    pygame.mixer.music.load(songs[0])
+    pygame.mixer.music.play()
+
 while True:
     key = ord(getch())
     if key == 27: #ESC
         break
     if key == 13: #Enter
-        print('enter!')
-    if key == 80: #Down arrow
-        pygame.mixer.music.stop()
-    if key == 72: #Up arrow
         pygame.mixer.music.play()
-
-
+    if key == 8: #Backspace
+        pygame.mixer.music.stop()
+    if key == 80: #Down arrow
+        play_last_song()
+    if key == 72: #Up arrow
+        play_next_song()
+    
 
 
 # Keep the program running until the music finishes
 while pygame.mixer.music.get_busy():
-    pygame.time.Clock().tick(10)
+    pygame.time.Clock().tick(100)
 
