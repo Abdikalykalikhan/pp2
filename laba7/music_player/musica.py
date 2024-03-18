a7f62bf4c4be1781649b3a593f2409d4ea5c6cff
Alikhan Abdikalyk <a_abdikalyk@kbtu.kz>
2024-03-18 12:33:27 +0600

    laba7

--- laba7/music_player/musica.py
+++ laba7/music_player/musica.py
@@ -4,18 +4,6 @@ import pygame
 songs = ['song.mp3', 'Arcade.mp3', 'Another_Love.mp3', 'Sweater_Weather.mp3']
 pygame.mixer.init()
 
-def play_next_song():
-    global songs
-    songs = songs[1:] + [songs[0]] # move current song to the back of the list
-    pygame.mixer.music.load(songs[0])
-    pygame.mixer.music.play()
-
-def play_last_song():
-    global songs
-    songs = [songs[-1]] + songs[:-1] # move current song to the back of the list
-    pygame.mixer.music.load(songs[0])
-    pygame.mixer.music.play()
-
 while True:
     key = ord(getch())
     if key == 27: #ESC
@@ -25,9 +13,13 @@ while True:
     if key == 8: #Backspace
         pygame.mixer.music.stop()
     if key == 80: #Down arrow
-        play_last_song()
+        songs = [songs[-1]] + songs[:-1] # move current song to the back of the list
+        pygame.mixer.music.load(songs[0])
+        pygame.mixer.music.play()
     if key == 72: #Up arrow
-        play_next_song()
+        songs = songs[1:] + [songs[0]] # move current song to the back of the list
+        pygame.mixer.music.load(songs[0])
+        pygame.mixer.music.play()
     
 
 
