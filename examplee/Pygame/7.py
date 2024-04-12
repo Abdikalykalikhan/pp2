a6f4080b97291a53ecee08021d06c867c1449b6f
Alikhan Abdikalyk <a_abdikalyk@kbtu.kz>
2024-04-13 02:42:32 +0600

    laba9

--- examplee/Pygame/7.py
+++ examplee/Pygame/7.py
@@ -32,6 +32,19 @@ font = pygame.font.SysFont("Verdana", 60)
 font_small = pygame.font.SysFont("Verdana", 20)
 game_over = font.render("Game Over", True, BLACK)
 
+
+
+
+#
+score_label = font_small.render("Score:", True ,BLACK)
+
+coins_label = font_small.render("Coins:", True ,BLACK)
+#
+
+
+
+
+
 background = pygame.image.load("AnimatedStreet.png")
 
 # Create a white screen
@@ -63,10 +76,10 @@ class Player(pygame.sprite.Sprite):
 
     def move(self):
         pressed_keys = pygame.key.get_pressed()
-        if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -10)
-        if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 10)
+        #if pressed_keys[K_UP]:
+        #    self.rect.move_ip(0, -10)
+        #if pressed_keys[K_DOWN]:
+        #    self.rect.move_ip(0, 10)
 
         if self.rect.left > 0:
             if pressed_keys[K_LEFT]:
@@ -100,24 +113,26 @@ class Small_Coin(pygame.sprite.Sprite):
         self.rect.move_ip(0, SPEED)
         if self.rect.top > 600:
             self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
 # Setting up Sprites
 P1 = Player()
 E1 = Enemy()
-C1 = Coin()  
+C1 = Coin() 
 C2 = Small_Coin()
+
 
 # Creating Sprites Groups
 enemies = pygame.sprite.Group()
 enemies.add(E1)
-coins = pygame.sprite.Group() 
-# Group for coins
+coins = pygame.sprite.Group()  # Group for coins
 coins.add(C1)
 smallcoins = pygame.sprite.Group()
 smallcoins.add(C2)
+
 all_sprites = pygame.sprite.Group()
 all_sprites.add(P1)
 all_sprites.add(E1)
-all_sprites.add(C1)
+all_sprites.add(C1)  # Adding the coin sprite to all_sprites
 all_sprites.add(C2)
 
 # Adding a new User event
@@ -153,10 +168,16 @@ while True:
 
         DISPLAYSURF.fill(RED)
         DISPLAYSURF.blit(game_over, (30, 250))
+        
+        # 
+
         final_score_text = font_small.render("score please:" + str(SCORE), True, BLACK)
         final_coins_text = font_small.render("coinsss please:" + str(COINS_COLLECTED), True, BLACK)
         DISPLAYSURF.blit(final_score_text, (30 , 320))
         DISPLAYSURF.blit(final_coins_text, (30, 350))
+
+        # 
+
         pygame.display.update()
         for entity in all_sprites:
             entity.kill()
@@ -166,12 +187,14 @@ while True:
 
     # Check for collision between Player and Coin
     if pygame.sprite.spritecollideany(P1, coins):
-        COINS_COLLECTED += 1  # Increment the coin count
+        COINS_COLLECTED += 3  # Increment the coin count
         coin_sound.play() # Play the coin collection sound
         C1.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)  # Relocate the coin
+
     if pygame.sprite.spritecollideany(P1, smallcoins):
         COINS_COLLECTED += 1  # Increment the coin count
         coin_sound.play() # Play the coin collection sound
         C2.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0) 
+
     pygame.display.update()
     FramePerSec.tick(FPS)
